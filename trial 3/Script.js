88340471e1f1b720f153cf6e88dcb7d58c178220
NisreenKhansa <159494740+NisreenKhansa@users.noreply.github.com>
2024-02-09 02:44:29 +0200

    Add files via upload

---
+++ trial 3/Script.js
@@ -0,0 +1,19 @@
+// Additional code for Research Papers and Books Lists
+const researchPapersList = document.querySelector('.research-papers ul');
+const booksList = document.querySelector('.books ul');
+
+// Research Papers
+const researchPapers = [
+    { title: 'Proceedings of the Sixth Arabic Natural Language Processing Workshop', link: 'https://aclanthology.org/volumes/2021.wanlp-1/' },
+    { title: 'A Panoramic Survey of Natural Language Processing in the Arab World', link: 'https://cacm.acm.org/magazines/2021/4/251355-a-panoramic-survey-of-natural-language-processing-in-the-arab-world/abstract' },
+    { title: 'Arabic Natural Language Processing: An Overview', link: 'https://www.sciencedirect.com/science/article/pii/S1319157818310553' }
+];
+
+// Books
+const books = [
+    { title: 'Introduction to Arabic Natural Language Processing', link: 'https://books.google.com.lb/books/about/Introduction_to_Arabic_Natural_Language.html?id=sYhyEAAAQBAJ&redir_esc=y' },
+    { title: 'Recent Advances in NLP: The Case of Arabic Language', link: 'https://link.springer.com/book/10.1007/978-3-030-34614-0' },
+    { title: 'Introduction to Arabic Natural Language Processing', link: 'https://link.springer.com/book/10.1007/978-3-031-02139-8' }
+];
+
+
